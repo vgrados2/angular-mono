f871f3a035f03ef29b99ecb016abdc2b65d23601
vgrados <vgrados2@outlook.com>
2020-05-10 17:13:35 -0500

    cambios

--- projects/linked/src/lib/linked.service.ts
+++ projects/linked/src/lib/linked.service.ts
@@ -1,10 +1,9 @@
 import { Injectable } from '@angular/core';
-import { SecondService } from 'second';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LinkedService {
 
-  constructor(private service: SecondService) { }
+  constructor() { }
 }
